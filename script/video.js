01d0f2d51e342245e0a40c3ea74f6efd26925708
syma sultana <symasultana2002@gmail.com>
2025-02-12 13:36:58 +0600

    create the card body

--- script/video.js
+++ script/video.js
@@ -45,17 +45,25 @@ const displayVideos = (videos) =>{
     card.classList = "card card-compact ";
       card.innerHTML = 
       `
-      <figure>
+      <figure class=" h-[200px] ">
     <img
       src=${video.thumbnail}
-      alt="Shoes" />
+      class="w-full h-full object-cover"
+      alt="Shoes"  />
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="card-body px-0 py-2 flex gap-2">
+    <div>
+    <img  class="w-10 h-10 rounded-full object-cover " src=${video.authors[0].profile_picture}
+     alt="card-body"  />
     </div>
+  <div>
+  <h2 class="font-bold">${video.title}</h2>
+  <div class="flex items-center gap-2">
+  <p class="text-gray-400">${video.authors[0].profile_name}</p>
+  <img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>
+  </div>
+  <p></p>
+  </div>
   </div>
       
       `;
