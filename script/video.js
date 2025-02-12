45c91164818340b1ea2116b1e088a9e92acbfe61
syma sultana <symasultana2002@gmail.com>
2025-02-12 14:45:33 +0600

    add the posteda_date

--- script/video.js
+++ script/video.js
@@ -45,11 +45,13 @@ const displayVideos = (videos) =>{
     card.classList = "card card-compact ";
       card.innerHTML = 
       `
-      <figure class=" h-[200px] ">
+      <figure class=" h-[200px]  relative">
     <img
       src=${video.thumbnail}
       class="w-full h-full object-cover"
       alt="Shoes"  />
+      ${video.others.posted_date ?.length == 0? "" : `<span class="absolute right-2 bottom-2 rounded bg-black text-white p-2">${video.others.posted_date}</span>`}
+      
   </figure>
   <div class="card-body px-0 py-2 flex gap-2">
     <div>
@@ -60,7 +62,9 @@ const displayVideos = (videos) =>{
   <h2 class="font-bold">${video.title}</h2>
   <div class="flex items-center gap-2">
   <p class="text-gray-400">${video.authors[0].profile_name}</p>
-  <img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>
+
+  ${video.authors[0].verified=== true  ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>`: ""}
+  
   </div>
   <p></p>
   </div>
---
+++ test.js
@@ -0,0 +1,9 @@
+const isVerified = "";
+// if(isVerified === true){
+//     console.log("the user is varified");
+// }
+// else{
+//     console.log("the user is  not varified");  
+// }
+
+console.log(`${isVerified === true ? "the user is varified": "the user is  not varified"}`)
