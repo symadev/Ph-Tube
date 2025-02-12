e1521952bbdf2ce8ac81039105d8a56e4672acd8
syma sultana <symasultana2002@gmail.com>
2025-02-12 20:02:53 +0600

    edit the search ber

--- script/video.js
+++ script/video.js
@@ -25,8 +25,8 @@ const loadCategories =() =>{
       .then(data => displayCategories(data.categories))
       .catch((error) => console.log(error));
 };
-const loadVideos =() =>{
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+const loadVideos =(searchText = "") =>{
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
       .then(res => res.json())
       .then(data =>displayVideos(data.videos))
       .catch((error) => console.log(error));
@@ -181,6 +181,11 @@ const displayCategories = (categories) =>{
     });
     
 };
+document.getElementById("search-input").addEventListener("keyup" ,(e) => {
+
+  loadVideos(e.target.value);
+
+});
 
 
 loadCategories ();
